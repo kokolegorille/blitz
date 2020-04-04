9630c9c477b7355adf43c268925985dd94f29557
kokolegorille <koko.le.gorille@gmail.com>
2020-04-04 23:45:34 +0200

    Initial commit

---
+++ lib/blitz.ex
@@ -0,0 +1,18 @@
+defmodule Blitz do
+  @moduledoc """
+  Documentation for `Blitz`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blitz.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/blitz/application.ex
@@ -0,0 +1,19 @@
+defmodule Blitz.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Blitz.Worker.start_link(arg)
+      # {Blitz.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Blitz.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
